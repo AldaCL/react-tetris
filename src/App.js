37bf7033cb475be8869d00215dc5d63ef74fa2d2
Aldair <aldair.alda27@gmail.com>
2019-09-16 23:21:19 -0500

    Aber si ahora si

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tetris from './components/Tetris';
+
+const App =() => (
+  <div className= "App">
+    <Tetris></Tetris>
+  </div>
+
+);
+
+
+export default App;
---
+++ src/components/Stage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+import Cell from './Cell';
+
+const Stage = ({stage})=> (
+    <div> <Cell/> </div>
+)
+
+
+export default Stage ;
+
+
